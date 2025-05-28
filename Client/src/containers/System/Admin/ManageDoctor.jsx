87d69e88ec69394557f5766ed92565537b564226
LyTruongsinh <23021410@vnu.edu.vn>
2025-05-28 21:50:09 +0700

    change

--- Client/src/containers/System/Admin/ManageDoctor.jsx
+++ Client/src/containers/System/Admin/ManageDoctor.jsx
@@ -2,55 +2,88 @@ import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import * as actions from "../../../store/actions";
 import { connect } from "react-redux";
-// import "./ManageDoctor.scss";
+import "./ManageDoctor.scss";
 import MarkdownIt from "markdown-it";
 import MdEditor from "react-markdown-editor-lite";
-// import style manually
 import "react-markdown-editor-lite/lib/index.css";
+import Select from "react-select";
 
-// Register plugins if required
-// MdEditor.use(YOUR_PLUGINS_HERE);
-
-// Initialize a markdown parser
 const mdParser = new MarkdownIt(/* Markdown-it options */);
-// Finish!
-function handleEditorChange({ html, text }) {
-    console.log("handleEditorChange", html, text);
-}
+const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+];
 class ManageDoctor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userRedux: [],
+            contentMarkdown: "",
+            contentHTML: "",
+            selectedOption: "",
+            description: "",
         };
     }
-    componentDidMount = () => {
-        this.props.fetchUserRedux();
+    componentDidMount = () => {};
+    componentDidUpdate = (prevProps, prevState) => {};
+    handleEditorChange = ({ html, text }) => {
+        this.setState({
+            contentMarkdown: text,
+            contentHTML: html,
+        });
     };
-    componentDidUpdate = (prevProps, prevState) => {
-        if (prevProps.listUsers !== this.props.listUsers) {
-            this.setState({
-                userRedux: this.props.listUsers,
-            });
-        }
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption }, () =>
+            console.log(`Option selected:`, this.state.selectedOption)
+        );
     };
-    handleDeleteUser = (user) => {
-        this.props.deleteUserRedux(user);
+    handleSaveContentMarkdown = () => {
+        console.log("hoi dan it check state", this.state);
     };
-    handleEditUser = (user) => {
-        this.props.handleEditUserFromParentKey(user);
+    handleOnChangeDescription = (event) => {
+        this.setState({
+            description: event.target.value,
+        });
     };
     render() {
-        let arrUsers = this.state.userRedux;
         return (
-            <React.Fragment>
-                <div>Tạo Thêm thông tin doctor</div>
-                <MdEditor
-                    style={{ height: "500px" }}
-                    renderHTML={(text) => mdParser.render(text)}
-                    onChange={handleEditorChange}
-                />
-            </React.Fragment>
+            <div className="manage-doctor-container">
+                <div className="manage-doctor-title">Tạo Thêm thông tin doctor</div>
+                <div className="more-infor">
+                    <div className="content-left form-group">
+                        <label>Chọn bác sĩ</label>
+                        <Select
+                            value={this.state.selectedOption}
+                            onChange={this.handleChange}
+                            options={options}
+                        />
+                    </div>
+                    <div className="content-right">
+                        <label>Thông tin giới thiệu</label>
+                        <textarea
+                            className="form-control"
+                            rows="4"
+                            onChange={(event) => this.handleOnChangeDescription(event)}
+                            value={this.state.description}
+                        >
+                            hhhhh
+                        </textarea>
+                    </div>
+                </div>
+                <div className="manage-doctor-editor">
+                    <MdEditor
+                        style={{ height: "500px" }}
+                        renderHTML={(text) => mdParser.render(text)}
+                        onChange={this.handleEditorChange}
+                    />
+                </div>
+                <button
+                    className="save-content-doctor"
+                    onClick={() => this.handleSaveContentMarkdown()}
+                >
+                    Lưu thông tin
+                </button>
+            </div>
         );
     }
 }
